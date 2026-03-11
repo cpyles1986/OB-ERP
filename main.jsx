ccbe45907fc679baf7934eed67f8d5caa9b7c031
Charlie P <78687970+cpyles1986@users.noreply.github.com>
2026-03-11 15:34:35 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './OB-ERP.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
